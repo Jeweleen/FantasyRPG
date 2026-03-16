31770f3d0774a0c1e361d19b0baf5cb1c4a0dc85
Jeweleen <jeweleen.vaughn@gmail.com>
2026-03-16 10:32:32 -0400

    updated race in order to make sure option selected is of available race, no cat people yet :/

--- something.py
+++ something.py
@@ -2,7 +2,15 @@ from create_player import Player
 
 
 player_name = input("create player name, input:")
-player_race = input("create player race, input")
+#Updating so that a creater can only select from available races
+races = ["human", "elf", "orc"]
+
+player_race = input("Select race (human/elf/orc): ").lower()
+
+while player_race not in races:
+    player_race = input("Invalid race. Choose human, elf, or orc: ").lower()
 
 new_player = Player(player_name, player_race)
 new_player.printPlayer()
+
+
